e0e6336767de7ea99887e4ff9f8c6f39d459d52f
iloveooz <iloveooz@yandex.ru>
2019-01-10 16:03:09 +0700

    примеры для работы с парами элементовЭ

---
+++ max.cxx
@@ -0,0 +1,16 @@
+	// max example
+	#include <iostream>     // std::cout
+	#include <algorithm>    // std::max
+
+	int main () {
+	  std::cout << "max(1, 2) == " << std::max(1,2) << '\n';
+	  std::cout << "max(2, 1) == " << std::max(2,1) << '\n';
+	  std::cout << "max('a', 'z') == " << std::max('a','z') << '\n';
+	  std::cout << "max(3.14, 2.73) == " << std::max(3.14,2.73) << '\n';
+	  
+	  std::cout << "max(45, 55) == " << std::max(45, 55) << '\n'; 
+	  
+	  return 0;
+	}
+
+		
